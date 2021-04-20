95d7003ef926d6144400e5ca549732c7fa512712
Nail <ifns6193@gmail.com>
2021-04-21 01:52:01 +0300

    1 lesson

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фиьмов вы уже посмотрели?', '');
 
-console.log('arr' + '- object');
-console.log(4 + +'9');
+const personalMovieDB = {
+    count:numberOfFilms, 
+    movies: {}, 
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-let incr = 10,
-    decr = 10;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// ++incr;
-// --decr; 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(incr++);
-console.log(decr--);
+console.log(personalMovieDB);
